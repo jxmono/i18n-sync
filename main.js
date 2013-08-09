bc4879074a302327b876f5f908b2921b1ebfb19e
Ionica Bizau <bizauionica@yahoo.com>
2013-08-09 15:00:01 +0300

    Add translate function.

--- main.js
+++ main.js
@@ -6,5 +6,73 @@ module.exports = function(config) {
     var self = this;
     Events.call(self, config);
 
-    // TODO
+    /*
+     *  Translates the messages emited by modules
+     *  =========================================
+     *
+     *  config.translations is an array of objects
+     *  in the following format:
+     *
+     *  [
+     *      {
+     *          "old": "I am.",
+     *          "new": "Je suis."
+     *      },
+     *      {
+     *          "old": "You are.",
+     *          "new": {
+     *              "fr": "Tu es.",
+     *              "ro": "Tu ești."
+     *          }
+     *      },
+     *      ...
+     *  ]
+     *
+     * */
+    self.translate  = function (messageToTranslate, callback) {
+
+        try {
+            messageToTranslate = JSON.parse(messageToTranslate);
+        } catch (e) {}
+
+        config.translations = config.translations || [];
+
+        var message;
+
+        if (messageToTranslate instanceof Object) {
+            if (messageToTranslate.message) {
+                message = messageToTranslate.message;
+            }
+        }
+
+        if (!message) {
+            return console.error("The message is not in the i18n format.");
+        }
+
+        for (var i = 0; i < config.translations.length; ++i) {
+            var current = config.traslations[i];
+
+            if (current["new"] instanceof Object) {
+                current["new"] = current["new"][config.lang || M.getLocale()];
+            }
+
+            if (current["old"] === message) {
+                message = current["new"];
+                break;
+            }
+        }
+
+        messageToTranslate.message = message;
+        callback(null, messageToTranslate);
+    };
+
+    // listen to crud events
+    if (config.listen instanceof Array) {
+        for (var i = 0; i < config.listen.length; ++i) {
+            self.on("message", config.listen[i], self.translate);
+        }
+    }
+
+
+    self.emit("ready", config);
 };
